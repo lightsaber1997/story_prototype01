a97ae580fb6810082652fe0e06d844af3da88fad
Seohyun Lee <applemint2003@gmail.com>
2025-08-16 12:18:09 +0900

    Remove unused variable

--- app/core/app_state.py
+++ app/core/app_state.py
@@ -1,6 +1,5 @@
 
 class AppState:
     def __init__(self):
-        self.user_selected_image_path: str | None = None
-        self.story_title: str = ""
+        self.image_path: str | None = None
         self.pages = {}
